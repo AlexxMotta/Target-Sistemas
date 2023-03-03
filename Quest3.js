f1ecfd6946cbbc723f7fa147a4eda26387e48bd2
AlexxMotta <alexmota@alu.ufc.br>
2023-03-03 13:38:14 -0300

    terminando questão 3

--- Quest3.js
+++ Quest3.js
@@ -1,7 +1,7 @@
 console.log("3) Descubra a lógica e complete o próximo elemento:")
-console.log("a) 1, 3, 5, 7, 9")// Números impares
-console.log("b) 2, 4, 8, 16, 32, 64, 128")// Potencias de base 2
-console.log("c) 0, 1, 4, 9, 16, 25, 36, 49")// A razão é o conjunto dos números primos
-console.log("d) 4, 16, 36, 64, 90")// a razão começa em 12 e vai aumentando de 8 em 8.
-console.log("e) 1, 1, 2, 3, 5, 8, 13")// Sequência Fibonacci, está faltando o 0 incial.
-console.log("f) 2, 10, 12, 16, 17, 18, 19, ____")// Potencias de base 2
+console.log("a) 1, 3, 5, 7 - 9")// Números impares
+console.log("b) 2, 4, 8, 16, 32, 64 - 128")// Potencias de base 2
+console.log("c) 0, 1, 4, 9, 16, 25, 36 - 49")// A razão é o conjunto dos números primos
+console.log("d) 4, 16, 36, 64 - 90")// a razão começa em 12 e vai aumentando de 8 em 8.
+console.log("e) 1, 1, 2, 3, 5, 8 - 13")// Sequência Fibonacci, está faltando o 0 incial.
+console.log("f) 2, 10, 12, 16, 17, 18, 19 - 200")// os número começam a letra D.
